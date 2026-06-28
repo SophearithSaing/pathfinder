19e76b35dced3a7cec9ff7e4b1503b99230909ff
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-28 21:43:07 +0700

    Add AppForm component

---
+++ src/components/AppForm.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface AppFormEmits {
+  submit: [event: SubmitEvent];
+}
+
+const emit = defineEmits<AppFormEmits>();
+</script>
+
+<template>
+  <form class="app-form" novalidate @submit.prevent="emit('submit', $event)">
+    <slot></slot>
+  </form>
+</template>
+
+<style scoped>
+.app-form {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-3);
+}
+</style>

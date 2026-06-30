f461e839de24d6075a7c72705a50c957e67fa9ea
SophearithSaing <sophearithsaing123@gmail.com>
2026-07-01 01:31:33 +0700

    Add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+import AppHeader from './components/AppHeader.vue';
+
+const route = useRoute();
+const shouldShowHeader = computed<boolean>(() => route.name !== 'login');
+</script>
 
 <template>
+  <AppHeader v-if="shouldShowHeader" />
   <RouterView />
 </template>
